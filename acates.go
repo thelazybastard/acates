ff43e7a793e8b660506e7f2a7df8f77bd5a1cf7c
thelazybastard <256585194+thelazybastard@users.noreply.github.com>
2026-05-06 13:53:01 +0700

    finished tracker page

--- acates.go
+++ acates.go
@@ -5,6 +5,7 @@ import (
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -16,8 +17,34 @@ func main() {
 		widget.NewLabel("HEllo world"),
 	)
 
-	trackerPage := container.NewVBox(
-		widget.NewLabel("Hello world"),
+	trackerPage := container.NewVBox()
+	trackerPage.Add(
+		widget.NewButton("Add", func() {
+			entry := widget.NewEntry()
+			entry.SetPlaceHolder("Enter Ingredient here....")
+
+			addedIngredient := []*widget.FormItem{
+				widget.NewFormItem("Item", entry),
+			}
+
+			dialog.ShowForm("Add ingredient", "Add", "Cancel", addedIngredient, func(ok bool) {
+				if ok && entry.Text != "" {
+
+					contents := container.NewHBox()
+
+					label := widget.NewLabel(entry.Text)
+					button := widget.NewButton("Delete", func() {
+						contents.Hide()
+					})
+
+					contents.Add(label)
+					contents.Add(button)
+					trackerPage.Add(contents)
+
+					trackerPage.Refresh()
+				}
+			}, acatesWindow)
+		}),
 	)
 
 	suggestionsPage := container.NewVBox(
